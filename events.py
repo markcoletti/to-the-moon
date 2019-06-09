edeccbcedbe0b5665d0a55d4a86c315281ac8352
Mark Coletti <mcoletti@gmail.com>
2019-06-09 11:28:01 -0400

    First version for daily events that works.  Now to tweak.

--- events.py
+++ events.py
@@ -54,9 +54,41 @@ def process_daily(daily_events):
     :param daily_events: *sorted* dataframe by date and starting time
     :return: None
     """
+    def print_event(event):
+        print('\\vbox{')
+        print('\subsection*{',event.Title,'}',sep='')
+        print('\\begin{description}[leftmargin=6em,noitemsep,style=nextline]')
+        print('   \item[Location:]', event.Location)
+        if not pd.isnull(event.Host):
+            print('    \item[Host:]', event.Host)
+        if event['Start Time'] != 'Ongoing event':
+            print('    \item[Start time:]', event['Start Time'])
+        if not pd.isnull(event['End Time']):
+            print('    \item[End time:]', event['End Time'])
+        print('\end{description}\n')
+        print(latexize(event.Description),'\n')
+        if not pd.isnull(event.Notes):
+            print(latexize(event.Notes))
+
+        print('}\n')
+
+    # We use this to track when we switch over to a new day.  If the last date is not the same as the current, then
+    # we know we are starting a new day.
+    last_date = daily_events.Date[0]
+
+    print('\section*{',last_date,'}\n',sep='')
 
     for index, event in daily_events.iterrows():
-        print(index, event.Date, event['Start Time'], event['End Time'])
+        # print(index, event.Date, event['Start Time'], event['End Time'])
+
+        if event.Date != last_date:
+            # We've rolled over to a new day
+            last_date = event.Date
+            print('\section*{', last_date, '}\n', sep='')
+
+        print_event(event)
+
+
 
 
 if __name__ == '__main__':
