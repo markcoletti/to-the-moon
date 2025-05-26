1b9024c1f23a727dd179b2592fe5fcd08c77731e
Mark Coletti <mcoletti@gmail.com>
2025-05-26 14:32:35 -0400

    Start of keys to latex utility

---
+++ mk_keyfile.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+""" Used to generate LaTeX for art or camp map keys.
+
+Usage: mk_keyfile.py <CSV file>
+
+Output would be to stdout.  You should redirect output to desired target file.
+"""
